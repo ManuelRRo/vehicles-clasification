5c291ae1478230efdaec8dc8de5a32b55b7438e1
ManuelRRo <ds23906@gmail.com>
2025-10-08 00:05:35 -0600

    polygone2 zone fixed

--- speedAnalysisCv2/streamMain.py
+++ speedAnalysisCv2/streamMain.py
@@ -15,9 +15,24 @@ model = YOLO("traffic_analysis.pt")
 
 # Inicializar el tracker y los anotadores
 tracker = sv.ByteTrack()
+
+polygon = np.array([[699, 549], [768, 549], [778, 760], [696, 760]])
+#polygon2 = np.array([[619, 307], [625, 434], [952, 437], [937, 309]])
+polygon2 = np.array([[1219, 557], [1225, 934], [1852, 937], [1837, 559]])
+polygon3 = np.array([[608, 319], [677, 319], [675, 11], [589, 9]])
+
+zone = sv.PolygonZone(polygon=polygon)
+zone2 = sv.PolygonZone(polygon=polygon2)
+zone3 = sv.PolygonZone(polygon=polygon3)
+
 box_annotator = sv.RoundBoxAnnotator()
 label_annotator = sv.LabelAnnotator()
 trace_annotator = sv.TraceAnnotator()
+
+zone_annotator = sv.PolygonZoneAnnotator(zone=zone, color=sv.Color.WHITE, thickness=1, text_thickness=1, text_scale=1)
+zone_annotator2 = sv.PolygonZoneAnnotator(zone=zone2, color=sv.Color.WHITE, thickness=1, text_thickness=1, text_scale=1) 
+zone_annotator3 = sv.PolygonZoneAnnotator(zone=zone3, color=sv.Color.WHITE, thickness=1, text_thickness=1, text_scale=1)
+
 
 # --------------------------
 # Función de procesamiento
@@ -27,6 +42,10 @@ def process_frame(frame: np.ndarray) -> np.ndarray:
     detections = sv.Detections.from_ultralytics(results)
     detections = tracker.update_with_detections(detections)
 
+    detections_in_polygon_1 = zone.trigger(detections=detections)
+    detections_in_polygon_2 = zone2.trigger(detections=detections)
+    detections_in_polygon_3 = zone3.trigger(detections=detections)
+
     labels = [
         f"#{tracker_id} {results.names[class_id]}"
         for class_id, tracker_id in zip(detections.class_id, detections.tracker_id)
@@ -35,6 +54,9 @@ def process_frame(frame: np.ndarray) -> np.ndarray:
     annotated_frame = box_annotator.annotate(frame.copy(), detections=detections)
     annotated_frame = label_annotator.annotate(annotated_frame, detections=detections, labels=labels)
     annotated_frame = trace_annotator.annotate(annotated_frame, detections=detections)
+    annotated_frame = zone_annotator.annotate(scene=annotated_frame)
+    annotated_frame = zone_annotator2.annotate(scene=annotated_frame)
+    annotated_frame = zone_annotator3.annotate(scene=annotated_frame)
 
     return annotated_frame
 
